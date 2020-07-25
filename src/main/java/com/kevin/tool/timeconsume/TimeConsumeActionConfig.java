1fc1acf335e12b6ad5cafb4148ff04387e88aebd
lihongmin <li89hong05min17>
2020-07-25 09:39:29 +0800

    订单修改9

--- src/main/java/com/kevin/tool/timeconsume/TimeConsumeActionConfig.java
+++ src/main/java/com/kevin/tool/timeconsume/TimeConsumeActionConfig.java
@@ -1,7 +1,6 @@
 package com.kevin.tool.timeconsume;
 
 
-import lombok.extern.slf4j.Slf4j;
 import org.springframework.boot.autoconfigure.condition.ConditionalOnMissingBean;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
